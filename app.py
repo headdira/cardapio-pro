dec5d7b8f0c68a331bc66e502c9a7bf5870ac92c
gerson.moreira <gerson.moreira@LOOVI.INTERNO.LOOVI.TEC.BR>
2026-02-05 09:45:06 -0300

    upload do sistema v1.2

--- app.py
+++ app.py
@@ -1412,4 +1412,4 @@ if __name__ == '__main__':
     print("   • Sessions configuradas")
     print("=" * 50)
     
-    app.run(debug=True, port=5000, use_reloader=False)
+    app.run(host="0.0.0.0", port=5000)
